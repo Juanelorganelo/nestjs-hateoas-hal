11243f48f9288790554cb700be59a5e98c2a0662
Juan Manuel <manuel.garcia@ironbit.com.mx>
2020-06-07 22:45:01 -0500

    feat: add app entrypoint

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SnakeNamingStrategy } from "@app/core/typeorm/typeorm.naming-strategy";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: [".env", ".env.local"],
+        }),
+        TypeOrmModule.forRoot({
+            namingStrategy: new SnakeNamingStrategy(),
+        }),
+    ],
+})
+export class AppModule {}
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { AppModule } from "./app.module";
+
+/**
+ * Start the application.
+ * This resolves dependencies and starts the server.
+ */
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = app.get(ConfigService);
+
+    const host = config.get<string>("HOST", "0.0.0.0");
+    const port = config.get<number>("PORT", 8080);
+
+    await app.listen(port, host);
+}
+
+bootstrap();
