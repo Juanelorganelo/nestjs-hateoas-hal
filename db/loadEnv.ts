e49acfaddf9fbd39c027414014c0bea58f7f78bd
Juan Manuel <manuel.garcia@ironbit.com.mx>
2020-06-09 15:01:38 -0500

    fix(migrations): make loadEnv script sync

--- db/loadEnv.ts
+++ db/loadEnv.ts
@@ -1,27 +1,15 @@
+import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
-import { constants, promises as fs } from "fs";
 
 const names = [".env.local", ".env"];
 
-/**
- * Load environment variables from .env.* files.
- */
-async function loadEnv(): Promise<void> {
-    for (const name of names) {
-        const file = path.join(process.cwd(), name);
+for (const name of names) {
+    const file = path.join(process.cwd(), name);
 
-        try {
-            // eslint-disable-next-line no-await-in-loop
-            await fs.access(file, constants.F_OK);
-            dotenv.config({ path: file });
-        } catch (err) {
-            // eslint-disable-next-line no-console
-            console.error(err.message);
-            // eslint-disable-next-line no-process-exit
-            process.exit(1);
-        }
+    // There is no async method for exists.
+    // eslint-disable-next-line no-sync
+    if (fs.existsSync(file)) {
+        dotenv.config({ path: file });
     }
 }
-
-loadEnv();
